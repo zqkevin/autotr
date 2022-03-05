dcc8e9eb5d52b14da04065fe236a35d47174c674
zqkevin <71193655+zqkevin@users.noreply.github.com>
2022-03-05 17:57:46 +0800

    0.3.0 add api

--- automain.py
+++ automain.py
@@ -1,12 +1,8 @@
 from scrip import wdglob, command, okex, analy, makepr, binance, database
 from datetime import datetime
 import threading, time, log
-
-
 exitFlag = 0
 threads = []
-
-
 class Threadtimer(threading.Thread):
     def __init__(self, threadID, name):
         threading.Thread.__init__(self)
